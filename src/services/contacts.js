9761f4fe51165259be0f7d27ebedda5612e611f4
kulikp1 <pashakylik2@gmail.com>
2024-08-31 19:53:43 +0300

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,6 +7,7 @@ export const getAllContacts = async ({
   sortOrder = 'asc',
   sortBy = '_id',
   filter = {},
+  userId,
 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
@@ -20,6 +21,8 @@ export const getAllContacts = async ({
   if (typeof filter.isFavourite === 'boolean') {
     contactsQuery.where('isFavourite').equals(filter.isFavourite);
   }
+
+  contactsQuery.where('userId').equals(userId);
 
   const [contactsCount, contacts] = await Promise.all([
     ContactsCollection.find().merge(contactsQuery).countDocuments(),
@@ -38,30 +41,34 @@ export const getAllContacts = async ({
   };
 };
 
-export const getContactById = async (id) => {
-  return await ContactsCollection.findById(id);
+export const getContactById = async ({ _id, userId }) => {
+  return await ContactsCollection.findOne({ _id, userId });
 };
 
 export const createContact = async (payload) => {
   return await ContactsCollection.create(payload);
 };
 
-export const updateContact = async (contactId, payload, options = {}) => {
+export const updateContact = async ({contactId, userId}, payload, options = {}) => {
   const contactToUpdate = await ContactsCollection.findOneAndUpdate(
-    { _id: contactId },
+    {
+      _id: contactId,
+      userId
+     },
     payload,
     {
       new: true,
-      includeResultMetadata: true,
       ...options,
     },
   );
-  if (!contactToUpdate || !contactToUpdate.value) return null;
-  return {
-    contact: contactToUpdate.value,
-  };
+
+  if (!contactToUpdate) return null;
+  return contactToUpdate;
 };
 
-export const deleteContact = async (contactId) => {
-  return await ContactsCollection.findOneAndDelete({ _id: contactId });
+export const deleteContact = async ({ contactId, userId }) => {
+  return await ContactsCollection.findOneAndDelete({
+      _id: contactId,
+      userId
+     });
 };
